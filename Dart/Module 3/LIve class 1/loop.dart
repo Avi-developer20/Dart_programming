f33ed011b9d9df0c3c11cc455584938f50f2072f
Avi-developer20 <chakroborty177@gmail.com>
2025-04-14 20:59:15 +0600

    second commit

--- Dart/Module 3/LIve class 1/loop.dart
+++ Dart/Module 3/LIve class 1/loop.dart
@@ -2,27 +2,27 @@ main(){
   //for loop
   List<String> name = ["Avi", "Susmoy", "Sushanta", "Koushik", "Toha", "Anik"];
   List<double> result = [5.00, 4.90, 4.95, 4.50, 4.98, 4.49];
-  // String grate = "";
-  // for(int i = 0; i < name.length; i++){
-  //   if(result[i] == 5.00){
-  //     grate = "A+";
-  //   }else if (result[i] == 4.90 || result[i] == 4.95 || result[i] == 4.98){
-  //     grate = "A";
-  //   }else if(result[i] == 4.50){
-  //     grate = "A-";
-  //   }else{
-  //     grate = "Fail";
-  //   }
-  //   print("Student name is: ${name[i]} \nYour result ${result[i]}\nand point $grate");
-  // }
+  String grate = "";
+  for(int i = 0; i < name.length; i++){
+    if(result[i] == 5.00){
+      grate = "A+";
+    }else if (result[i] == 4.90 || result[i] == 4.95 || result[i] == 4.98){
+      grate = "A";
+    }else if(result[i] == 4.50){
+      grate = "A-";
+    }else{
+      grate = "Fail";
+    }
+    print("Student name is: ${name[i]} \nYour result ${result[i]}\nand point $grate");
+  }
 
 
-  //While loop
-  // int i =0;
-  // while(i <name.length){
-  //   print("student name is : ${name[i]}  \nYour result ${result[i]}");
-  //   i++;
-  // }
+  While loop
+  int i =0;
+  while(i <name.length){
+    print("student name is : ${name[i]}  \nYour result ${result[i]}");
+    i++;
+  }
 
 
   //do_while loop
