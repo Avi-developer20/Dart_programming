473e44c5c19d73ecb6b3ab0f16d846142a14d082
Avi-developer20 <chakroborty177@gmail.com>
2025-04-14 21:38:06 +0600

    first branch commit

---
+++ Dart/git_command/git.dart
@@ -0,0 +1,35 @@
+//Git repository file push command
+// 1. git init
+// 2. git add . / filename
+// 3. git commit -m ""
+// 4. git file link
+// 5. git push -u origin main
+//
+// // add After update project
+// 1. git add . /filename
+// 2. git commit -m ""
+// 3. git push
+//
+// //Details check
+// 1. git log --oneline
+// 2. git log --oneline --all
+//
+// //Branching
+// 1.gti branch feature (create branch)
+// 2. git checkout feature (Trancper)
+// 3. git swith feature (Trancper)
+// 4. git branch (check branch)
+// 5. git checkout -b new brach name (Transper and swith)
+// 6. git status (check status)
+// 7. git commit -m ""
+// 8. git push -u origin new branch name
+//
+// //Fetch
+// 1. git fetch
+//
+//
+// //Unstage/remove
+// 1. git rest filename( file remove / remove)
+// 2. git reset
+// 3. git reset --hard
+//
